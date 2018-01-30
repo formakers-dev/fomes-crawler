034d8d335d4c2257428df78c718c785a6fd6997c
Steve Jeong and Tiffany Kim <pair+optyyie+yoonyoungkim89@gmail.com>
2018-01-30 15:06:15 +0900

    오타 삭제

--- appbee_crawler/util/date_util.py
+++ appbee_crawler/util/date_util.py
@@ -3,7 +3,7 @@ class DateUtil(object):
 
     @staticmethod
     def get_date_format(date):
-        pattern = r'(\d+)년[ ]*(\d+)월[ ]*(\d+)일'
+        pattern = '(\d+)년[ ]*(\d+)월[ ]*(\d+)일'
         r = re.compile(pattern)
         match = r.search(date)
         return match.group(1) + match.group(2).zfill(2) + match.group(3).zfill(2)
