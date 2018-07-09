d6100f9ce3557d4b2513774a506011e6fe508e95
Jake Kim <copyx00@gmail.com>
2018-07-09 17:26:37 +0900

    Uncrawled App Request 에 return 대신 yield 적용

--- appbee_crawler/spiders/uncrawled_app_spider.py
+++ appbee_crawler/spiders/uncrawled_app_spider.py
@@ -5,22 +5,26 @@ from scrapy.spidermiddlewares.httperror import HttpError
 from appbee_crawler.manager.db_manager import DBManager
 from appbee_crawler.spiders.parser.app_item_parser import AppItemParser
 
+
 class UncrawledAppSpider(scrapy.Spider):
     name = "UncrawledAppSpider"
     allowed_domains = ["play.google.com"]
 
     def start_requests(self):
         uncrawled_apps_list = DBManager.select_uncrawled_apps()
-        request_list = []
         for app in uncrawled_apps_list:
-            packageName = app['packageName']
-            request_list.append(scrapy.Request('https://play.google.com/store/apps/details?id=' + packageName, callback=self.after_parsing, meta={'packageName': packageName}, dont_filter=True, errback=self.error_handler))
-        return request_list
+            package_name = app['packageName']
+            request = scrapy.Request('https://play.google.com/store/apps/details?id=' + package_name,
+                                     callback=self.after_parsing, meta={'packageName': package_name},
+                                     dont_filter=True, errback=self.error_handler)
+            yield request
 
-    def after_parsing(self, response):
+    @staticmethod
+    def after_parsing(response):
         yield AppItemParser.parse(response)
 
-    def error_handler(self, failure):
+    @staticmethod
+    def error_handler(failure):
         if failure.check(HttpError):
             response = failure.value.response
             print(response.meta.get('packageName'), "/", response.status)
