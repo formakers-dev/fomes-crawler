d3923ecb0aafbed3cd762c5a67516a9de7ae65a7
Jake Kim <copyx00@gmail.com>
2019-01-07 18:26:35 +0900

    test: trim 테스트 코드 리팩토링
    - 주어진 인자의 타입별 테스트코드 추가
    - 트림 결과 검증 함수 네이밍 변경: __check_not_trimmed => __verify_trimmed
    
    https://trello.com/c/orModB8X

--- appbee_crawler/test/string_util_test.py
+++ appbee_crawler/test/string_util_test.py
@@ -1,6 +1,8 @@
 import re
 import unittest
 
+import scrapy
+
 from appbee_crawler.app_items import AppItem
 from appbee_crawler.category_items import CategoryItem
 from appbee_crawler.util.string_util import StringUtil
@@ -9,75 +11,95 @@ from appbee_crawler.util.string_util import StringUtil
 class StringUtilTest(unittest.TestCase):
     def setUp(self):
         super().setUp()
-        self.app_item = AppItem()
-        self.category_item = CategoryItem()
         self.trimmedPattern = re.compile('(^[\s\uFEFF]+|[\s\uFEFF]+$)')
 
-    def test_parse_number(self):
+    def test_parse_number_호출시__입력된_숫자_문자열에서_공백과_쉼표를_제거_후_정수로_바꿔서_반환한다(self):
         self.assertEqual(StringUtil.parseNumber("100,000"), 100000)
         self.assertEqual(StringUtil.parseNumber(" 111,222,333 "), 111222333)
 
-    def __check_not_trimmed(self, obj):
-        not_trimmed_count = 0
-
+    def __verify_trimmed(self, obj):
         if type(obj) is str:
-            if self.trimmedPattern.match(obj):
-                print(obj, 'is not trimmed')
-                not_trimmed_count += 1
+            self.assertEqual(self.trimmedPattern.match(obj), None)
         elif type(obj) is list:
             for item in obj:
-                not_trimmed_count += self.__check_not_trimmed(item)
-        elif type(obj) is dict:
+                self.__verify_trimmed(item)
+        elif type(obj) is dict or isinstance(obj, scrapy.Item):
             for value in obj.values():
-                not_trimmed_count += self.__check_not_trimmed(value)
-        elif type(obj) is AppItem:
-            for value in obj.values():
-                not_trimmed_count += self.__check_not_trimmed(value)
+                self.__verify_trimmed(value)
 
-        return not_trimmed_count
+    def test_trim_호출시__주어진_문자열의_앞_뒤_공백문자들과_BOM문자_제거(self):
+        arg_string = "\t\n\t  \r\tFAMILY_ACTION    \uFEFF \uFEFF \uFEFF"
 
-    def test_trim_with_app_item(self):
-        self.app_item['appName'] = ' 마이 오아시스 - 힐링되는 하늘섬 키우기                   \t \t \t '
-        self.app_item['packageName'] = '\n\t         com.buffstudio.myoasis '
-        self.app_item['appPrice'] = 0
-        self.app_item['categoryId1'] = 'GAME_SIMULATION\n'
-        self.app_item['categoryName1'] = '시뮬레이션'
-        self.app_item['categoryId2'] = ' FAMILY_ACTION    \uFEFF \uFEFF \uFEFF'
-        self.app_item['categoryName2'] = '\n액션/어드벤처 '
-        self.app_item['contentsRating'] = '\n만 3세 이상 '
-        self.app_item['description'] = '\uFEFF탭해서 하트를 모아 다양한 동물들이 뛰노는 오아시스'
-        self.app_item['developer'] = '  Buff Studio Co.Ltd.             \uFEFF\t\t'
-        self.app_item['star'] = 4.649204254150391
-        self.app_item['installsMin'] = 1000000
-        self.app_item['installsMax'] = 5000000
-        self.app_item['reviewCount'] = 99833
-        self.app_item['updatedDate'] = "20180629    "
-        self.app_item['inappPriceMin'] = 1100
-        self.app_item['inappPriceMax'] = 110000
-        self.app_item['similarApps'] = list()
-        self.app_item['similarApps'] = []
-        self.app_item['similarApps'].append("  com.idleif.abyssrium\t")
-        self.app_item['similarApps'].append("\ncom.idleif.abyssrium\t")
-        self.app_item['similarApps'].append("      com.idleif.abyssrium")
-        self.app_item['iconUrl'] = "\t\t\t          https://lh3.googleusercontent.com/OJ12eDDC6zShVguWb2mBS--cdUDRy4BzyqR_BfTy8kG5ibNwsbYbibNUnEW-hxUMlUM=s180\n\n\n"
+        result = StringUtil.trim(arg_string)
 
-        self.app_item['imageUrls'] = []
-        self.app_item['imageUrls'].append("https://lh3.googleusercontent.com/TVyiUM_oGq1IRvMWdQNA-mr8Vl4WdkEqdiObLfYadyyF9OoTHSZT8AnsvYFFxffkTw=w720-h310\uFEFF")
-        self.app_item['imageUrls'].append("\uFEFF  https://lh3.googleusercontent.com/JWAY26HZZwFGW5LwgGJaqYe5CXGePh_oLNPtRwNGZnLpa8n-MDnVeEGs0y9ALAMyNv4=w720-h310  ")
-        self.app_item['imageUrls'].append("\t\uFEFF\t\t     https://lh3.googleusercontent.com/voBPYesTu9PKPvOo8VKQc15YLO3hhRO-zSr-F-d5TlJcaJllI3QFKPdoEcMpxq5Xaf4=w720-h310\n\t")
+        self.__verify_trimmed(result)
 
-        self.app_item = StringUtil.trim(self.app_item)
+    def test_trim_호출시__주어진_리스트_내_모든_아이템들에_트림을_적용한다(self):
+        arg_list = [
+            ' 마이 오아시스 - 힐링되는 하늘섬 키우기                   \t \t \t ',
+            '\n\t         com.buffstudio.myoasis \r\n\r\f',
+            '\t\uFEFF\t\t     https://lh3.googleusercontent.com/voBPYesTu9PKPvOo8VKQc15YLO3hhRO-zSr-F-d5TlJcaJllI3QFKPdoEcMpxq5Xaf4=w720-h310\n\t'
+        ]
 
-        not_trimmed_count = self.__check_not_trimmed(self.app_item)
+        result = StringUtil.trim(arg_list)
 
-        self.assertEqual(not_trimmed_count, 0)
+        self.__verify_trimmed(result)
 
-    def test_trim_with_category_item(self):
-        self.category_item['id'] = '\n\t\r       \uFEFFGAME_ACTION                  '
-        self.category_item['title'] = '  액션       \n  \n  \n  \uFEFF'
+    def test_trim_호출시__주어진_딕셔너리_내_모든_값들에_트림을_적용한다(self):
+        arg_dict = dict()
+        arg_dict['appName'] = ' 마이 오아시스 - 힐링되는 하늘섬 키우기                   \t \t \t '
+        arg_dict['packageName'] = '\n\t         com.buffstudio.myoasis \r\t        \n\f'
+        arg_dict['appPrice'] = 0
+        arg_dict['categoryId1'] = 'GAME_SIMULATION\n'
+        arg_dict['categoryName1'] = '시뮬레이션'
+        arg_dict['categoryId2'] = '\t\n\r  \r\r\r  \r FAMILY_ACTION    \uFEFF \uFEFF \uFEFF'
+        arg_dict['categoryName2'] = '\n액션/어드벤처 '
+        arg_dict['contentsRating'] = '  \uFEFF\ufeff \n\f\r\n만 3세 이상 '
 
-        self.category_item = StringUtil.trim(self.category_item)
+        result = StringUtil.trim(arg_dict)
 
-        not_trimmed_count = self.__check_not_trimmed(self.category_item)
+        self.__verify_trimmed(result)
 
-        self.assertEqual(not_trimmed_count, 0)
+    def test_trim_호출시__주어진_AppItem_내_모든_필드들에_트림을_적용한다(self):
+        app_item = AppItem()
+        app_item['appName'] = ' 마이 오아시스 - 힐링되는 하늘섬 키우기                   \t \t \t '
+        app_item['packageName'] = '\n\t         com.buffstudio.myoasis '
+        app_item['appPrice'] = 0
+        app_item['categoryId1'] = 'GAME_SIMULATION\n'
+        app_item['categoryName1'] = '시뮬레이션'
+        app_item['categoryId2'] = ' FAMILY_ACTION    \uFEFF \uFEFF \uFEFF'
+        app_item['categoryName2'] = '\n액션/어드벤처 '
+        app_item['contentsRating'] = '\n만 3세 이상 '
+        app_item['description'] = '\uFEFF탭해서 하트를 모아 다양한 동물들이 뛰노는 오아시스'
+        app_item['developer'] = '  Buff Studio Co.Ltd.             \uFEFF\t\t'
+        app_item['star'] = 4.649204254150391
+        app_item['installsMin'] = 1000000
+        app_item['installsMax'] = 5000000
+        app_item['reviewCount'] = 99833
+        app_item['updatedDate'] = "20180629    "
+        app_item['inappPriceMin'] = 1100
+        app_item['inappPriceMax'] = 110000
+        app_item['similarApps'] = list()
+        app_item['similarApps'] = []
+        app_item['similarApps'].append("  com.idleif.abyssrium\t")
+        app_item['similarApps'].append("\ncom.idleif.abyssrium\t")
+        app_item['similarApps'].append("      com.idleif.abyssrium")
+        app_item['iconUrl'] = "\t\t\t          https://lh3.googleusercontent.com/OJ12eDDC6zShVguWb2mBS--cdUDRy4BzyqR_BfTy8kG5ibNwsbYbibNUnEW-hxUMlUM=s180\n\n\n"
+        app_item['imageUrls'] = []
+        app_item['imageUrls'].append("https://lh3.googleusercontent.com/TVyiUM_oGq1IRvMWdQNA-mr8Vl4WdkEqdiObLfYadyyF9OoTHSZT8AnsvYFFxffkTw=w720-h310\uFEFF")
+        app_item['imageUrls'].append("\uFEFF  https://lh3.googleusercontent.com/JWAY26HZZwFGW5LwgGJaqYe5CXGePh_oLNPtRwNGZnLpa8n-MDnVeEGs0y9ALAMyNv4=w720-h310  ")
+        app_item['imageUrls'].append("\t\uFEFF\t\t     https://lh3.googleusercontent.com/voBPYesTu9PKPvOo8VKQc15YLO3hhRO-zSr-F-d5TlJcaJllI3QFKPdoEcMpxq5Xaf4=w720-h310\n\t")
+
+        result = StringUtil.trim(app_item)
+
+        self.__verify_trimmed(result)
+
+    def test_trim_호출시__주어진_CategoryItem_내_모든_필드들에_트림을_적용한다(self):
+        category_item = CategoryItem()
+
+        category_item['id'] = '\n\t\r       \uFEFFGAME_ACTION                  '
+        category_item['title'] = '  액션       \n  \n  \n  \uFEFF'
+
+        result = StringUtil.trim(category_item)
+
+        self.__verify_trimmed(result)
